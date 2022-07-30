8023825365aa2463a0c7ef24c43aceb683b76563
Ewout Ghysbrecht <ewout.ghysbrecht@liantis.be>
2022-07-30 11:27:37 +0200

    updateQuality returns value, make items private, use descriptions instead of string values

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -1,62 +1,66 @@
 import {Item} from './item';
+import {Description} from './description.enum';
+import {MAX_QUALITY} from '@/gilded-tros.functions';
 
 export class GildedTros {
+    private _items: Array<Item> = new Array<Item>();
 
-    constructor(public items: Array<Item>) {
-
+    constructor(items: Array<Item>) {
+        this._items = items;
     }
 
-    public updateQuality(): void {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Good Wine' && this.items[i].name != 'Backstage passes for Re:Factor'
-                && this.items[i].name != 'Backstage passes for HAXX') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'B-DAWG Keychain') {
-                        this.items[i].quality = this.items[i].quality - 1;
+    public updateQuality(): Item[] {
+        for (let i = 0; i < this._items.length; i++) {
+            if (this._items[i].name != Description.GOOD_WINE && this._items[i].name != Description.BACKSTAGE_PASSES_FOR_RE_FACTOR
+                && this._items[i].name != Description.BACKSTAGE_PASSES_FOR_HAXX) {
+                if (this._items[i].quality > 0) {
+                    if (this._items[i].name != Description.B_DAWG_KEYCHAIN) {
+                        this._items[i].quality = this._items[i].quality - 1;
                     }
                 }
             } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1;
+                if (this._items[i].quality < MAX_QUALITY) {
+                    this._items[i].quality = this._items[i].quality + 1;
 
-                    if (this.items[i].name == 'Backstage passes for Re:Factor') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
+                    if (this._items[i].name == Description.BACKSTAGE_PASSES_FOR_RE_FACTOR) {
+                        if (this._items[i].sellIn < 11) {
+                            if (this._items[i].quality < MAX_QUALITY) {
+                                this._items[i].quality = this._items[i].quality + 1;
                             }
                         }
 
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
+                        if (this._items[i].sellIn < 6) {
+                            if (this._items[i].quality < MAX_QUALITY) {
+                                this._items[i].quality = this._items[i].quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (this.items[i].name != 'B-DAWG Keychain') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
+            if (this._items[i].name != Description.B_DAWG_KEYCHAIN) {
+                this._items[i].sellIn = this._items[i].sellIn - 1;
             }
 
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Good Wine') {
-                    if (this.items[i].name != 'Backstage passes for Re:Factor' || this.items[i].name != 'Backstage passes for HAXX') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'B-DAWG Keychain') {
-                                this.items[i].quality = this.items[i].quality - 1;
+            if (this._items[i].sellIn < 0) {
+                if (this._items[i].name != Description.GOOD_WINE) {
+                    if (this._items[i].name != Description.BACKSTAGE_PASSES_FOR_RE_FACTOR || this._items[i].name != Description.BACKSTAGE_PASSES_FOR_HAXX) {
+                        if (this._items[i].quality > 0) {
+                            if (this._items[i].name != Description.B_DAWG_KEYCHAIN) {
+                                this._items[i].quality = this._items[i].quality - 1;
                             }
                         }
                     } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality;
+                        this._items[i].quality = this._items[i].quality - this._items[i].quality;
                     }
                 } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
+                    if (this._items[i].quality < MAX_QUALITY) {
+                        this._items[i].quality = this._items[i].quality + 1;
                     }
                 }
             }
         }
+        return this._items;
     }
 
 }
