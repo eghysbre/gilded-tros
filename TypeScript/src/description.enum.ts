8d49460ffcb2a6fe17371ed8ea5cd5b9b5e82035
Ewout Ghysbrecht <ewout.ghysbrecht@liantis.be>
2022-07-30 11:22:15 +0200

    enum for descriptions and file for functions

---
+++ TypeScript/src/description.enum.ts
@@ -0,0 +1,7 @@
+export enum Description {
+    GOOD_WINE = 'Good Wine',
+    BACKSTAGE_PASSES_FOR_RE_FACTOR = 'Backstage passes for Re:Factor',
+    BACKSTAGE_PASSES_FOR_HAXX = 'Backstage passes for HAXX',
+    B_DAWG_KEYCHAIN = 'B-DAWG Keychain',
+    RING_OF_CLEANSENING_CODE = 'Ring of Cleansening Code'
+}
---
+++ TypeScript/src/gilded-tros.functions.ts
@@ -0,0 +1,2 @@
+export const MAX_QUALITY = 50;
+export const LEGENDARY_QUALITY = 80;
